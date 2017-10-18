b983b8f76111402579aef96ae29364cd07862f08
Zhang Ranrui <zhangranrui@gmail.com>
2017-10-18 01:27:41 -0700

    Modify 01.setup.sh

--- 01.setup.sh
+++ 01.setup.sh
@@ -2,11 +2,12 @@
 # This script will pull in the CHIP-SDK, and then unpack it.
 # Some things will be downloaded, compiled, and installed so it may ask for
 # your password.
+HERE="$PWD"
 
-if [ ! -d "$PWD/CHIP-SDK" ] ; then
+if [ ! -d "$HERE/CHIP-SDK" ] ; then
   git clone https://github.com/nextthingco/CHIP-SDK
 fi
-cd "$PWD/CHIP-SDK"
+cd "$HERE/CHIP-SDK"
 bash setup_ubuntu1404.sh
 
 sed -i 's/atenart/nextthingco/' CHIP-buildroot/package/dtc-overlay/dtc-overlay.mk
@@ -17,15 +18,7 @@ sudo apt-get install multistrap qemu-user-static live-build
 #patch multistrap
 sudo sed -i 's/\$forceyes //' /usr/sbin/multistrap
 
-cd -
-git clone https://github.com/NextThingCo/CHIP-linux.git
-cd "$PWD/CHIP-linux"
-git checkout nextthing/4.4/chi
+cd $HERE
+git clone https://github.com/NextThingCo/CHIP-linux.git --single-branch --branch nextthing/4.4/chip --depth 1
 
-cd "$PWD/CHIP-SDK/CHIP-buildroot"
-git apply $PWD/01.patch
-
-cd "$PWD/CHIP-linux"
-git apply $PWD/02.patch
-
-sed -i "/^BR2_LINUX_KERNEL_CUSTOM_LOCAL_PATH=/cBR2_LINUX_KERNEL_CUSTOM_LOCAL_PATH=\"$PWD/CHIP-linuxaaa\""  $PWD/CHIP-SDK/CHIP-buildroot/configs/chippro_defconfig
+sed -i "/^BR2_LINUX_KERNEL_CUSTOM_LOCAL_PATH=/cBR2_LINUX_KERNEL_CUSTOM_LOCAL_PATH=\"$HERE/CHIP-linux\""  $HERE/CHIP-SDK/CHIP-buildroot/configs/chippro_defconfig
